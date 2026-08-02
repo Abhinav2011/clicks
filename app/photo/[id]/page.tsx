ec0e7cae51aaf9e8bff6ebb8564f80032ec83187
Abhinav Kumar <abhi32eee@gmail.com>
2026-08-02 22:51:54 +0530

    seo optimisation

--- app/photo/[id]/page.tsx
+++ app/photo/[id]/page.tsx
@@ -57,8 +57,37 @@ export default async function PhotoPage({ params }: PageProps) {
     notFound();
   }
 
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.stillframes.net";
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "ImageObject",
+    contentUrl: photo.web_image_url || photo.thumbnail_url,
+    url: `${baseUrl}/photo/${photo.id}`,
+    name: photo.title || "Fujifilm Photograph",
+    description:
+      photo.description ||
+      `Photograph shot on ${photo.camera || "Fujifilm"} with ${photo.film_simulation || "film simulation"}.`,
+    author: {
+      "@type": "Person",
+      name: "Abhinav Kumar",
+    },
+    creator: {
+      "@type": "Person",
+      name: "Abhinav Kumar",
+    },
+    exifData: [
+      { "@type": "PropertyValue", name: "Camera", value: photo.camera },
+      { "@type": "PropertyValue", name: "Film Simulation", value: photo.film_simulation },
+      { "@type": "PropertyValue", name: "Lens", value: photo.lens },
+    ].filter((item) => Boolean(item.value)),
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-paper">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <Header />
 
       <main className="flex-1 py-8 sm:py-12">
